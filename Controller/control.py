4accae7993ac7a236b11a2f4dc3f794a8dce70a0
yuanyzh <yuanyzh@shanghaitech.edu.cn>
2016-05-22 09:06:26 +0800

    details

--- Controller/control.py
+++ Controller/control.py
@@ -1,4 +1,4 @@
-	# Python 3.4.3 #
+# Python 3.4.3 #
 from apscheduler.schedulers.blocking import BlockingScheduler
 from logic.models import Logic
 import django
@@ -8,32 +8,32 @@ import time
 
 def excute():
 	commands = Logic.objects.all()
-	time_present = time.time()
 	for command in commands:
+		time_present = time.time()
 		query = json.loads(command['Q'])
 		action = json.loads(command['A'])
 		time_interval = command['T']
 		time_stamp = command['TimeStamp']
-		if (time_stamp - time_interval) % time_interval >= 5:
+		if (time_present - time_stamp) % time_interval >= 5:
 			continue
-		
-		# WARNING: objects cannot be written in JSON
 		i = 0
 		while (i + 4 < len(query)):
-			API1 = API.objects.get(id = query[i]['API'].API)
-			API2 = API.objects.get(id = query[i + 2]['API'].API)
+			API1 = API.objects.get(id = query[i]['API'])
+			API2 = API.objects.get(id = query[i + 2]['API'])
 			tmp1 = requests.get(API1.provider.baseurl + API1.slug, data = query[i]['args'])
 			tmp2 = requests.get(API2.provider.baseurl + API2.slug, data = query[i + 2]['args'])
 			if API1.retu in ['int', 'float']:
 				flag = eval(tmp1 + query[i + 1] + tmp2)
 			else:
-				flag = (tmp1 == tmp2)
+				if qurey[i+1] == '=':
+					flag = (tmp1 == tmp2)
+				else:
+					flag = (tmp1 != tmp2)
 			if flag == False:
 				continue
 			i = i + 4
-		API1 = API.objects.get(id = action['API'].API)
-		requests.get(API1.provider.baseurl + action['API'].slug)
-
+		API1 = API.objects.get(id = action['API'])
+		requests.get(API1.provider.baseurl + API1.slug)
 
 sched = BlockingScheduler()
 sched.add_job(excute, 'interval', seconds = 5)
